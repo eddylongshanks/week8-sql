4fd845e0ce77f35bf1979aff916723a788c41c7e
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2020-11-27 16:46:52 +0000

    Add Portfolio Task 2

---
+++ app.py
@@ -0,0 +1,58 @@
+import sqlite3
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data/test_database.db"
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(80), nullable=False)
+    secondname = db.Column(db.String(80), nullable=False)
+    country = db.Column(db.String(32), nullable=False)
+
+    # Define __repr__ that will be called when querying e.g. 'Users.query.all()'
+    def __repr__(self):
+        obj_repr = f'ID: {self.id}, ' \
+                   f'First Name: {self.firstname}, ' \
+                   f'Second Name: {self.secondname}, ' \
+                   f'Country: {self.country}' \
+
+        return obj_repr
+
+@app.route("/")
+def index():
+    all_users = Users.query.all()
+    return render_template("index.html", all_users=all_users)
+
+@app.route("/add_user", methods=["GET"])
+def add_user():
+    return render_template("add_user.html")
+
+@app.route("/add_user", methods=["POST"])
+def add_user_post():
+
+    req = request.form
+
+    missing = list()
+
+    for k, v in req.items():
+        if v == "":
+            missing.append(k)
+
+    if missing:
+        feedback = f"Missing fields for {', '.join(missing)}"
+        return render_template("add_user.html", feedback=feedback)
+
+    new_user = Users(firstname=req['first-name'],
+                    secondname=req['second-name'],
+                    country=req['country'])
+
+    print(new_user)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/")
+
+app.run(debug=True)
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # SQL, plus sqlite3
 
-## Portfolio task
+## Portfolio Task 1
 
 - A short Python script that:
   - Creates and connects to an SQLite3 database
@@ -10,7 +10,18 @@
   - Prints results of SELECT query 
 - The actual SQLite3 database itself
 
-## More info
+Found in /task_1/
 
 ### db/database_created_from_sqlite3_cmdline.db 
+
 Database created with sqlite command line
+
+## Portfolio Task 2
+
+- A Basic Flask App (does not need to be new, you can use existing app)
+- App connects to SQLite3 database using SQLAlchemy
+- At least one example of a Model that represents a database table in your database
+- Model must contain appropriate __repr__ method
+- An example of a record being created in table using said Model
+- An example of querying database to return all records (can be printed or rendered in template)
+- The actual SQLite3 database itself
